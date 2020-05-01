efaadb0a3908f789b903cbe5b0b5f190e1250f09
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-01 15:06:30 +0200

    completo, manca il bonus

--- main.js
+++ main.js
@@ -3,22 +3,31 @@
 $('.next').click(function() {
     //creo la variabile img corrente con classe 'active' per recuperarla
     var img_corrente = $('img.active');
-    console.log(img_corrente);
+    //creo la variabile pallino corrente con classe 'active' per recuperarla
+    var pallino_corrente = $('.fa-circle.active');
     //tolgo la classe active all'immagine corrente per poterla poi applicare alla successiva
     img_corrente.removeClass('active');
+    //rimuovo da pallino la classe active per poterla poi applicare al pallino successivo
+    pallino_corrente.removeClass('active');
     //creo la variabile immagine successiva per recuperarla
     var img_successiva = img_corrente.next('img');
-    console.log(img_successiva);
+    //creo la variabile pallino successivo per recuperarlo
+    var pallino_successivo = pallino_corrente.next('.fa-circle')
 
     //verifico che esista una img successiva
 
     if (img_successiva.length != 0) {
-        //modifico l'img successiva con la classe active
+        //esiste l'img successiva quindi modifico l'img successiva con la classe active
         img_successiva.addClass('active');
+        //modifico il pallino successivo con la classe active
+        pallino_successivo.addClass('active');
     } else {
-        //torno alla prima img e applico la classe active
+        //non c'è un'img successiva allora torno alla prima img e applico la classe active
         img_successiva = $('img:first-child');
         img_successiva.addClass('active');
+        //modifico pallino pallino_successivo
+        pallino_successivo = $('.fa-circle:first-child');
+        pallino_successivo.addClass('active');
     }
 });
 
