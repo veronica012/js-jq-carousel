e36debfc5e0e8e2eb28adc5938cb7d056609f65b
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-04 18:08:32 +0200

    autoplay non funziona

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 var clock;
 //ogni click sulla freccia next (destra) si visualizza l'immagine successiva
 //bisogna intercettare il click e ad ogni click si visualizzerà l'immagine successiva che avrà classe active
-$('.next').click(function(){
+$('.next').click(function() {
     next_image();
 });
-
-
 //lo stesso procedimento si applica al click sulla freccia prev a sinistra
 //bisogna intercettare il click e ad ogni click si visualizzerà l'immagine precedente che avrà classe active
 $('.prev').click(function() {
@@ -39,6 +37,7 @@ $('.prev').click(function() {
 //rendere i pallini cliccabili
 //intercettare il click sui pallini
   $('.dots i').click(function(){
+
     //creo la variabile per recupera l'indice_corrente
     var indice_corrente = $(this).index();
     //associo l'indice corrente all'immagine corrispondente creando la variabile elemento corrente
@@ -53,12 +52,15 @@ $('.prev').click(function() {
   });
 
 
-//intercettare il click su button #start
-  $('#start').click(function(){
-      //var slideIndex = 0;
+//click su button #start
+  $('#start').click(function() {
       autoplay();
-
   });
+//click su button #pause
+  $('#pause').click(function() {
+       clearTimeout(autoplay);
+  });
+
 
 
   function next_image() {
@@ -88,10 +90,8 @@ $('.prev').click(function() {
           pallino_successivo = $('.fa-circle:first-child');
           pallino_successivo.addClass('active');
       }
-
-
-
   }
+
 function autoplay() {
     clock = setTimeout(next_image, 2000); // l'immagine cambia ogni 2 secondi
 
